017f3d6047b7f8290f7f4cdd90a96b699681c966
‪Taha Al-rehany‬‏ <32350359+taharehany@users.noreply.github.com>
2025-01-01 20:52:18 +0200

    Update header.php

--- header.php
+++ header.php
@@ -37,7 +37,7 @@
 				<div class="collapse navbar-collapse" id="navbarSupportedContent">
 					<ul class="navbar-nav mx-auto mb-2 mb-lg-0">
 						<li class="nav-item">
-							<a class="nav-link active" aria-current="page" href="#">الرئيسية</a>
+							<a class="nav-link active" aria-current="page" href="<?php echo home_url('/'); ?>">الرئيسية</a>
 						</li>
 
 						<li class="nav-item">
