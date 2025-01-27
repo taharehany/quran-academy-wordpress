5344841103799b8d017e8c9953f467fbe0d1afce
‪Taha Al-rehany‬‏ <32350359+taharehany@users.noreply.github.com>
2025-01-27 22:04:02 +0200

    Update header.php to add meta tags

--- header.php
+++ header.php
@@ -15,6 +15,28 @@
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
 
 	<?php wp_head(); ?>
+
+	<!-- Google Tag Manager -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	
+	})(window,document,'script','dataLayer','GTM-K4BX4VRW');</script>
+	<!-- End Google Tag Manager -->
+
+	<!-- Google tag (gtag.js) -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-1NDCMGFQFT"></script>
+	<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+	
+	  gtag('config', 'G-1NDCMGFQFT');
+	</script>
 </head>
 
 <body>
